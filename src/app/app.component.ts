e0c7363d301f2f4b8fecd3e5058986c8620ec517
Robert Villarreal <robertv1979@gmail.com>
2017-08-08 15:02:31 -0700

    Add email list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    emailList = [{
+        email: 'rob@rob.com',
+        priority: 'high',
+        subject: 'This is an email',
+        content: 'Sup?'
+    },{
+        email: 'rob@rob.com',
+        priority: 'high',
+        subject: 'This is an email',
+        content: 'Sup?'
+    },{
+        email: 'rob@rob.com',
+        priority: 'high',
+        subject: 'This is an email',
+        content: 'Sup?'
+    }]
+    title = 'Welcome To Email List';
 }
